ad767d81230afb1a346993820f8004b6d5cced9d
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2019-12-29 18:43:13 +0200

    add product component

--- src/components/product-item/product.component.jsx
+++ src/components/product-item/product.component.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./product.styles.scss";
 
-export default function Product() {
+export default function Product({ name, imageUrl, price, id }) {
+   const addToCart = id => {
+      console.log(id);
+   };
+
    return (
-      <div className="product">
-         <img src="" alt="" />
+      <div className="product" onClick={() => addToCart(id)}>
+         <img src={imageUrl} alt="" />
+         <div className="details">
+            <div className="product-name">{name}</div>
+            <span className="product-price">{price}$</span>
+         </div>
       </div>
    );
 }
