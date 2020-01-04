25d878507df72c92c98bf628c0e2d9b8913dd749
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-04 03:35:51 +0200

    add user props to layout

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -5,7 +5,7 @@ import Footer from "../components/footer/footer.component.jsx";
 function defaultLayout(props) {
    return (
       <>
-         <Navbar />
+         <Navbar user={props.user} />
          {props.children}
          <Footer />
       </>
