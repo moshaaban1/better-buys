80e8999be7a13a8b1e33edd0251a3ab06553dd95
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-09 21:56:35 +0200

    add checkout page

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,7 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
+import { Container } from "@material-ui/core";
+import CheckoutItems from "../../components/checkout-items/checkout-items";
 
-export default class Checkout extends Component {
-   render() {
-      return <main>Checkout</main>;
-   }
+export default function Checkout() {
+   return (
+      <main>
+         <Container>
+            <div className="page-content">
+               <CheckoutItems />
+            </div>
+         </Container>
+      </main>
+   );
 }
