43ed5f221422117c45110d619559386374f62c36
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-11 17:52:48 +0200

    update selectCollectionsByNumber to count

--- src/redux/shop/shop.reselect.js
+++ src/redux/shop/shop.reselect.js
@@ -2,12 +2,28 @@ import { createSelector } from "reselect";
 
 const selectShop = state => state.shop;
 
-export const selectShopCollections = createSelector(
+export const selectCollections = createSelector(
    [selectShop],
    shop => shop.collections
 );
 
-export const selectCollectionProducts = createSelector(
-   [selectShopCollections],
-   shop => shop
-);
+export const selectCollectionsByCount = collectionCount =>
+   createSelector(
+      [selectCollections],
+      collections =>
+         collections.map(collection => ({
+            ...collection,
+            items: collection.items.slice(0, collectionCount)
+         }))
+   );
+
+export const selectCollection = collectionId =>
+   createSelector(
+      [selectCollections],
+      collections =>
+         collections.find(
+            collection =>
+               collection.title.toLocaleLowerCase() ===
+               collectionId.toLocaleLowerCase()
+         )
+   );
