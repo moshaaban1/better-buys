72706667dc66a0e6acdc2cc0aa53929f7c1ab84c
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-13 23:44:58 +0200

    add redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const config = {
+   key: "cart",
+   storage,
+   whitelist: ["cart"]
+};
+
+const persistedReducer = persistReducer(config, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(logger));
+
+export const persistor = persistStore(store);
 
 export default store;
