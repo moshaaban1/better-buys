6853f2f5424519ebc428a6f64f9f244909e53add
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-14 20:05:46 +0200

    add firebase api

--- src/firebase/firebase.api.js
+++ src/firebase/firebase.api.js
@@ -1,13 +1,40 @@
 import { db } from "./firebase.config";
 
-export const getCategories = db
-   .collection("categories ")
-   .get()
-   .then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-         console.log(doc.id, doc.data());
-      });
-   })
-   .catch(error => {
-      console.log(error);
+// export const getCategories = async () => {
+//    const collectionRef = db.collection("categories ");
+//    const querySnapshot = await collectionRef.get();
+//    const docs = querySnapshot.docs.map(doc => doc.data());
+//    console.log(docs);
+// };
+
+const getCollections = () => {
+   const collectionRef = db.collection("collections");
+   return collectionRef.get();
+};
+
+export const convertCollectionsSnapshotToMap = async () => {
+   const collectionsSnapshot = await getCollections();
+
+   collectionsSnapshot.docs.map(docSnapshot => {
+      const { title, items } = docSnapshot.data();
+
+      return {
+         id: docSnapshot.id,
+         routeName: title.toLowerCase(),
+         title,
+         items
+      };
    });
+};
+
+export const addCollectionAndDocuments = (collectionKey, objectToAdd) => {
+   const collectionRef = db.collection(collectionKey);
+   const batch = db.batch();
+
+   objectToAdd.forEach(doc => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, doc);
+   });
+
+   batch.commit();
+};
