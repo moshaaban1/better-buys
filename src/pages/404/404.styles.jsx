b55693f158f9da708d344105e8a98d481f62603a
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-14 13:33:06 +0200

    update not found page style

---
+++ src/pages/404/404.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const NotFoundContainer = styled.div`
+   text-align: center;
+   font-weight: bold;
+`;
+
+export const Title = styled.h1`
+   font-size: 70px;
+`;
+
+export const PathName = styled.span`
+   font-size: 30px;
+`;
--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 
 import Button from "../../components/button/button.component";
-import "./404.style.scss";
+import { NotFoundContainer, Title, PathName } from "./404.styles";
 
 const NotFound = ({ location, history }) => (
-   <main>
-      <div className="not-found">
-         <span className="path-name">{location.pathname} </span>
-         <h1 className="title">404 NOT FOUND</h1>
-         <Button inverted clicked={() => history.push("/")}>
-            Back to Home Page
-         </Button>
-      </div>
-   </main>
+   <NotFoundContainer>
+      <PathName>{location.pathname} </PathName>
+      <Title>404 NOT FOUND</Title>
+      <Button inverted clicked={() => history.push("/")}>
+         Back to Home Page
+      </Button>
+   </NotFoundContainer>
 );
 
 export default NotFound;
