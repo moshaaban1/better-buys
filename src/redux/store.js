d485a39ff5aa4a55a2e1c71b595741bb1740421b
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-18 11:34:12 +0200

    add redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-import rootReducer from "./root";
+import thunk from "redux-thunk";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
+import rootReducer from "./root";
 
 const config = {
    key: "cart",
@@ -12,7 +14,7 @@ const config = {
 
 const persistedReducer = persistReducer(config, rootReducer);
 
-const store = createStore(persistedReducer, applyMiddleware(logger));
+const store = createStore(persistedReducer, applyMiddleware(logger, thunk));
 
 export const persistor = persistStore(store);
 
