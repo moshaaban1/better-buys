33da4e461df969746d02dd30edaa58d111098af5
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2020-01-18 16:19:27 +0200

    replace signIn and signup class component to function component with hooks

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Container } from "@material-ui/core";
 
 import { auth, createUserProfile } from "../../firebase/firebase.auth";
 
 import Button from "../../components/button/button.component";
 import FormInput from "../../components/form-input/form-input.component";
-export default class SignUp extends Component {
-   state = {
+
+const SignUp = () => {
+   const [inputValues, setInputValues] = useState({
       name: "",
       email: "",
       password: "",
       confirmPassword: ""
-   };
+   });
 
-   handleChange = e => {
+   const handleChange = e => {
       var { value, name } = e.target;
-      this.setState({ [name]: value });
+      setInputValues({ ...inputValues, [name]: value });
    };
 
-   handleSubmit = async e => {
+   const handleSubmit = async e => {
       e.preventDefault();
 
       const { name, email, password, confirmPassword } = this.state;
@@ -47,51 +48,51 @@ export default class SignUp extends Component {
       }
    };
 
-   render() {
-      return (
-         <Container>
-            <div className="sign-in">
-               <h2 className="title">I don't have an account</h2>
-               <span>Register a new account</span>
-               <form className="form" onSubmit={this.handleSubmit}>
-                  <FormInput
-                     type="text"
-                     name="name"
-                     value={this.state.name}
-                     onChange={this.handleChange}
-                     label="Name"
-                     required
-                  />
-                  <FormInput
-                     type="email"
-                     name="email"
-                     value={this.state.email}
-                     onChange={this.handleChange}
-                     label="Email"
-                     required
-                  />
-                  <FormInput
-                     type="password"
-                     name="password"
-                     value={this.state.password}
-                     onChange={this.handleChange}
-                     label="Password"
-                     required
-                  />
-                  <FormInput
-                     type="password"
-                     name="confirmPassword"
-                     value={this.state.confirmPassword}
-                     onChange={this.handleChange}
-                     label="Confirm Password"
-                     required
-                  />
-                  <Button inverted full>
-                     Sign Up
-                  </Button>
-               </form>
-            </div>
-         </Container>
-      );
-   }
-}
+   return (
+      <Container>
+         <div className="sign-in">
+            <h2 className="title">I don't have an account</h2>
+            <span>Register a new account</span>
+            <form className="form" onSubmit={handleSubmit}>
+               <FormInput
+                  type="text"
+                  name="name"
+                  value={inputValues.name}
+                  onChange={handleChange}
+                  label="Name"
+                  required
+               />
+               <FormInput
+                  type="email"
+                  name="email"
+                  value={inputValues.email}
+                  onChange={handleChange}
+                  label="Email"
+                  required
+               />
+               <FormInput
+                  type="password"
+                  name="password"
+                  value={inputValues.password}
+                  onChange={handleChange}
+                  label="Password"
+                  required
+               />
+               <FormInput
+                  type="password"
+                  name="confirmPassword"
+                  value={inputValues.confirmPassword}
+                  onChange={handleChange}
+                  label="Confirm Password"
+                  required
+               />
+               <Button inverted full>
+                  Sign Up
+               </Button>
+            </form>
+         </div>
+      </Container>
+   );
+};
+
+export default SignUp;
